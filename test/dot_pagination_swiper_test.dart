fd34c60ea06306bb0ca5f132f7108da12c6f735e
Seba <s@hetacode.pl>
2020-04-24 22:00:14 +0200

    added tests for setup dots colors and for check dots count depending on DotPaginationSwiper number of children elements

--- test/dot_pagination_swiper_test.dart
+++ test/dot_pagination_swiper_test.dart
@@ -13,6 +13,50 @@ const List<String> kStates = <String>[
 ];
 
 void main() {
+
+  testWidgets("One child element of DotPaginationSwiper should have one dot - active - with proper setup color", (tester) async {
+    await tester.pumpWidget(
+      Directionality(
+        textDirection: TextDirection.ltr,
+        child: DotPaginationSwiper(
+          activeDotColor: Colors.green,
+          inactiveDotColor: Colors.red,
+          children: <Widget>[
+            Container(),
+          ],
+        ),
+      ),
+    );
+
+    var pagination = find.byType(DotPagination);
+    var foundDots = find.descendant(of: pagination, matching: find.byType(ColorDot));
+    var dots = (foundDots.evaluate().map((m) => m.widget as ColorDot).toList());
+    expect(dots.length, equals(1));
+    expect(dots[0].color, isSameColorAs(Colors.green));
+  });
+  testWidgets("Two children elements of DotPaginationSwiper should have two dots - active and inactive - with proper setup colors", (tester) async {
+    await tester.pumpWidget(
+      Directionality(
+        textDirection: TextDirection.ltr,
+        child: DotPaginationSwiper(
+          activeDotColor: Colors.green,
+          inactiveDotColor: Colors.red,
+          children: <Widget>[
+            Container(),
+            Container(),
+          ],
+        ),
+      ),
+    );
+
+    var pagination = find.byType(DotPagination);
+    var foundDots = find.descendant(of: pagination, matching: find.byType(ColorDot));
+    var dots = (foundDots.evaluate().map((m) => m.widget as ColorDot).toList());
+    expect(dots.length, equals(2));
+    expect(dots[0].color, isSameColorAs(Colors.green));
+    expect(dots[1].color, isSameColorAs(Colors.red));
+  });
+
   testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
     final List<String> log = <String>[];
 
